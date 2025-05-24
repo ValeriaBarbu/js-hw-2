a8e2c00e6cab7157c93ab90a3e4614dd5f0c99b3
Valeria Barbu <valeriabarbu16@gmail.com>
2025-05-24 20:29:11 +0300

    FIrst commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let scoreDolphins = (96+108+89)/3;
+let scoreKoalas = (896666)/3;
+
+if(scoreDolphins>scoreKoalas){
+  console.log ("Delfinii castiga trofeul");
+}
+else {
+  if (scoreDolphins==scoreKoalas){
+    console.log ("Ambele echipe castiga trofeul")
+  }
+ else{
+   console.log ("Koalas castiga trofeul");
+
+ } 
+
+}
